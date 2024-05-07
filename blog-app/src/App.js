04eb7bb3eed56963afbe4ac5f15107d9aa5c8bc9
Dhvani Intwala <intdhvani2627@gmail.com>
2024-05-07 18:54:03 +0200

    Add nav bar with style

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+    <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">My World Travel</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link href="#home">Home</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Blog</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Destinations</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Categories</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Gallary</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">About</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    
+    </>
   );
 }
 
